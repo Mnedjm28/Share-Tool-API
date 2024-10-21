6f574e837c8d396fdc1a29e142d0bc3704034a5d
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-21 21:50:04 +0100

    Add project files

---
+++ ShareToolApi/ShareToolApi/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShareToolApi.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
