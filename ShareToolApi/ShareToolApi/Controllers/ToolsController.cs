d5f70187bd17e9760b443cfca055768dc4656653
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-21 22:35:40 +0100

    Add Tools controller and GetAllTools end point

---
+++ ShareToolApi/ShareToolApi/Controllers/ToolsController.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SharedToolApi.Business;
+using SharedToolApi.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace ShareToolApi.Controllers
+{
+    public class ToolsController : ApiController
+    {
+        public async Task<JsonResult<List<Tool>>> GetAllTools() {
+            var toolRepo = new ToolRepository();
+
+            return Json(await toolRepo.GetAllTools());
+        }
+    }
+}
