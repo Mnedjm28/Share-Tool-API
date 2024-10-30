cc809f3be1a79635ffe6bc16cbb78b0d8920f0ef
Ahmed Nedjm Eddine <89082915+Mnedjm28@users.noreply.github.com>
2024-10-31 00:01:46 +0100

    Add some tools end points

--- ShareToolApi/ShareToolApi/Controllers/ToolsController.cs
+++ ShareToolApi/ShareToolApi/Controllers/ToolsController.cs
@@ -3,6 +3,7 @@ using SharedToolApi.Business;
 using SharedToolApi.Business.DTOs;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -14,13 +15,42 @@ namespace ShareToolApi.Controllers
 {
     public class ToolsController : ApiController
     {
-        public async Task<JsonResult<List<ToolDto>>> GetAllTools() {
+        private ToolRepository _toolRepository { get; set; } = new ToolRepository();
+
+        [HttpGet]
+        public async Task<JsonResult<List<ToolDto>>> GetAllTools()
+        {
             var toolRepo = new ToolRepository();
             return Json(await toolRepo.GetAllTools(),
                 new JsonSerializerSettings()
                 {
                     ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
                 });
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Create()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, "Created successfuly");
+        }
+
+        [HttpPut]
+        public async Task<IHttpActionResult> Update(ToolDto tool)
+        {
+            //var imageUrl = await _toolRepository.Update(tool);            
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IHttpActionResult> Delete(int id)
+        {
+            var imageUrl = await _toolRepository.Delete(id);
+            if (imageUrl != null)
+            {
+                imageUrl = imageUrl.Replace("/Images/", string.Empty);
+                //File.Delete(Path.Combine(Server.MapPath("~/Images"), imageUrl));
+            }
+            return Ok();
         }
     }
 }
